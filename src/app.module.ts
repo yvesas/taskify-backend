8f9798436abd70afb7a627d41ade48c0e891bf4e
Yves <yves.as@gmail.com>
2025-03-25 11:24:15 -0300

    :sparkles: new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [DbModule, AuthModule, UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
